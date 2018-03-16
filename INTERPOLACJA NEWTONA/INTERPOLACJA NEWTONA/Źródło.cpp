77ab8ca9dad6a7195d4f37309828ba9802b93aa3
nastopendo <grzegorzlipski97@gmail.com>
2018-03-16 16:57:20 +0100

    uproszczono petle

--- INTERPOLACJA NEWTONA/INTERPOLACJA NEWTONA/Źródło.cpp
+++ INTERPOLACJA NEWTONA/INTERPOLACJA NEWTONA/Źródło.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	int n, i, j;
-	float x[size], y[size], a, sum = 0, mult;
+	double x[size], y[size], a, result = 0, mult;
 
 	cout << "Ile punktow: ";
 	cin >> n;
@@ -17,24 +17,26 @@ int main()
 		cout << "Podaj y[" << i << "] = ";
 		cin >> y[i];
 	}
-	cout << "\nWprowadz wartosc do interlopacji: ";
+	cout << "\nWprowadz wartosc do interpolacji: ";
 	cin >> a;
 
-	for (j = 0; j<n - 1; j++)
+	for (j = 0; j < n - 1; j++)
 	{
 		for (i = n - 1; i>j; i--)
 			y[i] = (y[i] - y[i - 1]) / (x[i] - x[i - j - 1]);
 	}
-	for (i = n - 1; i >= 0; i--)
+
+	for (i = 0; i < n; i++)
 	{
 		mult = 1;
-		for (j = 0; j<i; j++)
+		for (j = 0; j < i; j++)
 			mult *= (a - x[j]);
 
 		mult *= y[j];
-		sum += mult;
+		result += mult;
 	}
-	cout << "Wynik: " << sum;
+
+	cout << "Wynik: " << result << endl;
 	system("pause");
 	return 0;
 }
